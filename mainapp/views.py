77fd031387399b6e32b41089c05e18415a086cee
Vlad <mitelenager@mail.ru>
2022-02-20 15:23:47 +0300

    Third part

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from mainapp.models import Product, ProductCategory
 
 # Create your views here.
 
@@ -6,28 +7,36 @@ def index(request):
     context = {'title': 'главная'}
     return render(request, 'mainapp/index.html', context)
 
+# def products(request):
+#     context = {
+#         'title': 'каталог',
+#         'products': [
+#             {
+#                 'img': 'product-11',
+#                 'name': 'Супер лампа',
+#                 'description': 'Яркий свет.'
+#             },
+#             {
+#                 'img': 'product-21',
+#                 'name': 'Стул повышенного качества',
+#                 'description': 'Не оторваться.'
+#             },
+#             {
+#                 'img': 'product-31',
+#                 'name': 'Настольный светильник',
+#                 'description': 'Стильно.'
+#             }
+#
+#         ]
+#     }
+#     return render(request, 'mainapp/products.html', context)
+#
 def products(request):
     context = {
-        'title': 'каталог',
-        'products': [
-            {
-                'img': 'product-11',
-                'name': 'Супер лампа',
-                'description': 'Яркий свет.'
-            },
-            {
-                'img': 'product-21',
-                'name': 'Стул повышенного качества',
-                'description': 'Не оторваться.'
-            },
-            {
-                'img': 'product-31',
-                'name': 'Настольный светильник',
-                'description': 'Стильно.'
-            }
-
-        ]
+        'products': Product.objects.all(),
+        'categories':  ProductCategory.objects.all()
     }
+
     return render(request, 'mainapp/products.html', context)
 
 
